8d29afd4b249a054b38113662364e9dab47fe720
Katsumi Ono <katsumimarques@gmail.com>
2019-04-29 22:50:17 -0300

    improving EntryList with BaseResourceListComponent

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { BaseResourceListComponent } from "../../../shared/components/base-resource-list/base-resource-list.component";
 
@@ -10,27 +10,10 @@ import { EntryService } from "../shared/entry.service";
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-  entries: Entry[] = [];
-
-  constructor(private entryService: EntryService) { }
-
-  ngOnInit() {
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b) => b.id - a.id),
-      error => alert('Erro ao carregar a lista')
-    )
+  constructor(private entryService: EntryService) { 
+    super(entryService);
   }
 
-  deleteEntry(entry: Entry){
-    const mustDelete = confirm('Deseja realmente excluir este item? ');
-    
-    if (mustDelete) {
-      this.entryService.delete(entry.id).subscribe(
-        () => this.entries = this.entries.filter(element => element != entry),
-        () => alert('Erro ao tentar excluir!')
-      )
-    }
-  }
 }
